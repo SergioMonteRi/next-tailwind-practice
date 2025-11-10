0f846fb1aef1168bb764e61d5d6b7da683c24ad6
SergioMonteRi <sergioribeiropalermo@gmail.com>
2025-11-10 19:08:40 -0300

    feat: implement FileInput component for enhanced file upload functionality on homepage

---
+++ src/components/form/file-input/index.ts
@@ -0,0 +1,11 @@
+import { Control } from './components'
+import { ImagePreview } from './components'
+import { Root } from './components'
+import { Trigger } from './components'
+
+export const FileInput = {
+  Root,
+  Control,
+  Trigger,
+  ImagePreview,
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { Sidebar } from './sidebar'
 export { Input } from './input'
 export { SettingsTabs } from './settings-tabs'
+export { FileInput } from './form'
